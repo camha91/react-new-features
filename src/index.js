1b77eb69d6727e7036fad912a896f5289de32e65
camha91 <crystalhanguyen@gmail.com>
2020-10-13 15:15:51 -0700

    The useState Hook

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
+const App = (props) => {
+    const [count, setCount] = useState(props.count);
+
+    return (
+        <div>
+            <p>The current count is {count}</p>
+            <button onClick={() => setCount(count + 1)}>+1</button>
+            <button onClick={() => setCount(props.count)}>reset</button>
+            <button onClick={() => setCount(count - 1)}>-1</button>
+        </div>
+    )
+};
+
+App.defaultProps = {
+    count: 0
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <div>my new content</div>
+    <App count={2}/>
   </React.StrictMode>,
   document.getElementById('root')
 );

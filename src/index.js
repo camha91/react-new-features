89f363ae62c4bbbc26d329320d0a4dd4e5a14ed1
camha91 <crystalhanguyen@gmail.com>
2020-10-13 16:42:57 -0700

    Complex State with useState

--- src/index.js
+++ src/index.js
@@ -2,22 +2,41 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-const App = (props) => {
-    // const [count, setCount] = useState(props.count);
-    // const [text, setText] = useState('');
+const NoteApp = () => {
+    const [notes, setNotes] = useState([]);
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
 
-    const [state, setState] = useState({
-        count: props.count,
-        text: ''
-    });
+    const addNote = (e) => {
+        e.preventDefault();
+        setNotes([
+            ...notes,
+            { title, body }
+        ]);
+        setTitle('');
+        setBody('');
+    };
+
+    const removeNote = (title) => {
+        setNotes(notes.filter((note) => note.title !== title));
+    };
 
     return (
         <div>
-            <p>The current {state.text || 'count'} is {state.count}</p>
-            <button onClick={() => setState({ count: state.count - 1 })}>-1</button>
-            <button onClick={() => setState({ count: props.count })}>reset</button>
-            <button onClick={() => setState({ count: state.count + 1 })}>+1</button>
-            <input value={state.text} onChange={(e) => setState({ text: e.target.value })}/>
+            <h1>Notes</h1>
+            {notes.map((note) => (
+                <div key={note.title}>
+                    <h3>{note.title}</h3>
+                    <p>{note.body}</p>
+                    <button onClick={() => removeNote(note.title)}>x</button>
+                </div>
+            ))}
+            <p>Add note</p>
+            <form onSubmit={addNote}>
+                <input value={title} onChange={(e) => setTitle(e.target.value)}/>
+                <textarea value={body} onChange={(e) => setBody(e.target.value)}></textarea>
+                <button>add note</button>
+            </form>
         </div>
     )
 };
@@ -37,13 +56,9 @@ const App = (props) => {
 //     )
 // };
 
-App.defaultProps = {
-    count: 0
-};
-
 ReactDOM.render(
   <React.StrictMode>
-    <App count={2}/>
+    <NoteApp />
   </React.StrictMode>,
   document.getElementById('root')
 );

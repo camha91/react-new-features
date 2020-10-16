180c765f31c4a6bd3a452ecc74424919349c13dc
camha91 <crystalhanguyen@gmail.com>
2020-10-16 16:53:54 -0700

    The Context API & useContext Hook

---
+++ src/components/NoteApp.js
@@ -0,0 +1,32 @@
+import React, {useEffect, useReducer, useState} from 'react';
+import notesReducer from '../reducers/notes';
+import NoteList from '../components/NoteList';
+import AddNoteForm from '../components/AddNoteForm';
+import NotesContext from '../context/notes-context';
+
+const NoteApp = () => {
+    const [notes, dispatch] = useReducer(notesReducer, []);
+
+    useEffect(() => {
+        const notes = JSON.parse(localStorage.getItem('notes'));
+
+        if (notes) {
+            dispatch({ type: 'POPULATE_NOTES', notes })
+            // setNotes(notesData);
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('notes', JSON.stringify(notes));
+    }, [notes]);
+
+    return (
+        <NotesContext.Provider value={{ notes, dispatch }}>
+            <h1>Notes</h1>
+            <NoteList />
+            <AddNoteForm />
+        </NotesContext.Provider>
+    )
+};
+
+export { NoteApp as default };
